7665c7bb2c9311f6f71f9e9a250a2c9b2f15aa4d
Ran Lu <ranluoverflow@hotmail.com>
2017-05-11 11:25:08 +0800

    Automation refinement and appearance adjustment

--- flaskr/flaskr/0_lab_PC__pack.bat
+++ flaskr/flaskr/0_lab_PC__pack.bat
@@ -1,1 +1,4 @@
+pushd .
+cd D:\as_lab\flask_my\lab_project_X_rectangle_circle_2\flaskr\flaskr
 7z a -r ..\..\flaskr.7z .
+popd
--- flaskr/flaskr/1_remote_server__copy_and_unpack.bat
+++ flaskr/flaskr/1_remote_server__copy_and_unpack.bat
@@ -4,3 +4,4 @@ copy \\tsclient\D\as_lab\flask_my\lab_project_X_rectangle_circle_2\flaskr.7z .
 7z x flaskr.7z -y
 del flaskr.7z
 popd
+iisreset
