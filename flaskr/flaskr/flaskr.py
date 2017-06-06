f7675971809ca682d542ef86a33f8d6e3b892e74
Ran Lu <ranluoverflow@hotmail.com>
2017-06-06 17:13:12 +0800

    Clearify code and align implementation to requirements
    Previously, click any 'group' link in the admin_index view and hierarchy
    of all users will be presented.
    However, by the requirement, after clicking the 'group' link of a
    certain user row in the admin_index view, only the hierarchy of the
    specific user should be presented.

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -1266,13 +1266,13 @@ def admin_logout():
     return redirect(url_for('admin_login'))
 
 
-@app.route('/admin_user_hierarchy')
-def admin_user_hierarchy():
+@app.route('/admin_user_hierarchy/<entry_id>')
+def admin_user_hierarchy(entry_id):
     if not session.get('admin_logged_in'):
         abort(401)
     admin_account = session.get('admin_logged_in_account')
 
-    return render_template('admin_user_hierarchy.html')
+    return render_template('admin_user_hierarchy.html', entry_id=entry_id)
 
 def prepare_user_info_for_user_hierarchy(x):
     return str(x.UE_ID) + '[' + (u'已激活' if 1 == x.not_help else u'未激活') + ',' + x.UE_truename + ']'
